80718f7e06d7a1e571b8e1908d78dba550d35c31
Evan <Evan@R2Hs-MBP.attlocal.net>
2022-10-09 21:44:12 -0400

    add styling

--- product/json.js
+++ product/json.js
@@ -1,21 +1,21 @@
 //fetch grabs data across networks
 
 
-fetch('/music.data.json') 
-.then(response => response.json)
-.then(json => {
+fetch("music-data.json")
+  .then((response) => response.json())
+  .then((json) => {
 
     // console.log(json.Employees[0].jobTitle);
 
     // let list = json.Employees[0].jobTitle;
 
     // document.querySelector('ol').innerHTML = list;
-let list = "";
-    json.Products.forEach(Products => {
-        console.log(`beginning of loop${list}`)
-        list += `<li>${Products}</li>`
-        console.log(`end of loop ${list}`);
+    let products = "";
+    json.Products.forEach((Products) => {
+        products += `<div class="image"><img src="${Products.images}"></div>
+        <div class="name">${Products.productName}</div>
+        <div class="specs">${Products.specs}</div>
+        <div class="price">${Products.price}</div>`;
 });
-document.querySelector('ol').innerHTML = list;
-document.querySelector('purple').innerHTML = json.Products[0].name;
+document.querySelector("#productContainer").innerHTML = products;
 })
